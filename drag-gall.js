d7dccca7cc5854984025f565553947053a4879d2
SoRaang <dsc0320@hotmail.com>
2024-08-02 12:36:56 +0900

    Mid-save

--- drag-gall.js
+++ drag-gall.js
@@ -1,1 +1,34 @@
-const imgDragCont = document.getElementById('imgDragCont');
+/** 다른 간단한 드래그 & 드롭 예제 */
+
+const dragCont = document.getElementById('imgDragCont');
+const imgElems = dragCont.querySelectorAll('.img-element');
+
+let currentDrag = null;
+
+imgElems.forEach(img => {
+    img.addEventListener('dragstart', () => {
+        img.classList.add('dragging');
+        currentDrag = img;
+    });
+
+    img.addEventListener('dragend', () => {
+        img.classList.remove('dragging')
+    });
+
+    img.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        console.log('1')
+    });
+
+    img.addEventListener('drop', () => {
+        if (currentDrag !== img) {
+            dragCont.insertBefore(currentDrag, img);
+        }
+
+        currentDrag = null;
+    });
+});
+
+dragCont.addEventListener('dragover', (e) => {
+    e.preventDefault();
+});
--- index.js
+++ index.js
@@ -84,7 +84,7 @@ btnInsert.addEventListener('click', () => {
 const dragConts = document.querySelectorAll('.drag-container');
 const btnDrags = document.querySelectorAll('.button-draggable');
 
-btnDrags.forEach(btn => {
+btnDrags.forEach(btn => { // 드래그하는 중에 dragging 클래스를 부여한다.
     btn.addEventListener('dragstart', (e) => {
         btn.classList.add('dragging');
     });
@@ -97,25 +97,25 @@ btnDrags.forEach(btn => {
 dragConts.forEach(cont => {
     cont.addEventListener('dragover', (e) => {
         e.preventDefault();
-        let afterElement = getDragAfterElement(cont, e.clientX);
+        let afterElement = getDragAfterElement(cont, e.clientX); // 아래의 함수를 이용해 드래그 중인 요소가 다른 요소의 앞에 있는지 판별한다.
         let currentDrag = document.querySelector('.dragging');
 
-        if (afterElement === undefined) {
+        if (afterElement === undefined) { // 드래그 중인 요소가 맨 마지막이면 마지막 자식 요소로 append 한다.
             cont.appendChild(currentDrag);
-        } else {
+        } else { // 드래그 중인 요소가 다른 요소의 앞에 있으면, 그 앞에 insert 한다.
             cont.insertBefore(currentDrag, afterElement);
         }
     });
 });
 
 function getDragAfterElement(container, x) {
-    let currentChilds = [ ... container.querySelectorAll('.button-draggable:not(.dragging)') ];
+    let currentChilds = [ ... container.querySelectorAll('.button-draggable:not(.dragging)') ]; // 자신의 내부에서 드래그 중이 아닌 요소 선택
 
     return currentChilds.reduce((closest, child) => {
-        let box = child.getBoundingClientRect();
-        let offset = x - box.left - box.width / 2;
+        let box = child.getBoundingClientRect(); // 드래그 중이 아닌 요소들의 충돌 박스 크기
+        let offset = x - box.left - box.width / 2; // 상위 이벤트에서 x로 전달받은 인자는 e.clientX 이다. 즉, 마우스의 위치가 박스의 앞쪽 절반에 있는지를 판단한다.
 
-        if (offset < 0 && offset > closest.offset) {
+        if (offset < 0 && offset > closest.offset) { // reduce를 이용해 이전 요소의 offset보다 가까우면 현재 요소를, 아니면 이전 요소를 반환한다.
             return { offset: offset, element: child }
         } else {
             return closest;
@@ -125,4 +125,5 @@ function getDragAfterElement(container, x) {
 
 /**
  * 위의 getDragAfterElement 함수를 통해 드래그 위치에 따라 append할 위치를 정할 수 있다.
+ * 여기서는 가로로만 정렬되는 형태이기 때문에 offset은 x 좌표를 기준으로 계산한다.
  */
